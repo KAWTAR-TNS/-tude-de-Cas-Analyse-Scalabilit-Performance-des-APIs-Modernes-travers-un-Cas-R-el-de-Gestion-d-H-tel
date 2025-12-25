e6899a60b0ed4307631c84fc18e819c729f5c565
kawtar-tns <tanassakawtar@gmail.com>
2025-12-25 14:26:30 +0100

    cccc

---
+++ Backend/src/main/java/com/example/Etude/de/Cas/EtudeDeCasApplication.java
@@ -0,0 +1,13 @@
+package com.example.Etude.de.Cas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtudeDeCasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtudeDeCasApplication.class, args);
+	}
+
+}
